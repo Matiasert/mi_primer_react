18ded18b1e6205abc56a2b2232ed98247b183b00
Matiasert <monteslopezmatias1@gmail.com>
2025-07-28 16:03:11 -0500

    moncho

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Saludo from './saludo';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola React</h1>
+      <Saludo nombre="Santiago" />
+      <Saludo nombre="Laura" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/saludo.js
@@ -0,0 +1,6 @@
+import saludo from './saludo';
+
+function Saludo(props) {
+    return <h2>Hola {props.nombre}, ¡bienvenido a react!</h2>;
+}
+export default Saludo;
